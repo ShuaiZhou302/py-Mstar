bbfdbf11ba17ad7783f24e9ff4dcab46a357381a
davidzhou718 <davidzhou718@gmail.com>
2024-06-01 09:52:46 +0800

    update toy test

---
+++ toy_test.py
@@ -0,0 +1,20 @@
+from Library import *
+from Mstar import *
+
+"""
+A toy example
+and the optimal cost should be 53
+"""
+map_data = [[0, 1, 1, 1],
+ [0, 0, 0, 0],
+ [0, 1, 1, 1],
+ [0, 1, 1, 1],
+ [0, 1, 1, 1],
+ [0, 1, 1, 1]]
+#  input Choose tuple for it is hashable
+graph = Graph(6,4, map_data)
+Sinit = ((5, 0), (4, 0), (3, 0), (1, 0))
+Send = ((2, 0), (3, 0), (4, 0), (5, 0))
+
+planner = m_star(graph, Sinit, Send)
+print(planner.solve())
