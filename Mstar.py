4ce6ce17a0b38517e235ac7ab4f77186c5e3ad4f
davidzhou718 <davidzhou718@gmail.com>
2024-06-01 09:41:36 +0800

    didn't debug yet

--- Mstar.py
+++ Mstar.py
@@ -1,6 +1,7 @@
 """
 A naive implementation of M* algorithm
 """
+import copy
 import heapq
 from collections import deque
 
@@ -17,6 +18,11 @@ class joint_Node:
         self.config = config
         self.parent = parent
         self.g = g
+
+    def __hash__(self):
+        # 将states转换为元组，然后计算其哈希值
+        state_tuple = self.config
+        return hash(state_tuple)
 
 
 class m_star:
@@ -82,11 +88,24 @@ class m_star:
             f, state = heapq.heappop(self.Open)
             if state.config == self.v_f:
                 return self.back_track(state)
+            if state in self.closed:
+                continue
+            else:
+                self.closed.add(state)
 
             # get neighbours of a
-            Snbh = self.get_neighbours(state)
-            for s in Snbh:
+            Sngh = self.get_neighbours(state)
+            for s in Sngh:
                 self.Add_toBackprop(state, s)
+                C_i = self.collision_detect(s)
+                self.UnionColset(s,C_i)
+                self.backprop(state, C_i)
+
+                if self.compare(s, C_i):
+                    heapq.heappush(self.Open, (s.g + self.get_h(s), s))
+
+        print("So sorry bruh, it just fail")
+        return None
 
     def get_h(self, state):
         """
@@ -116,7 +135,26 @@ class m_star:
         may return a list of joint nodes to visit
         """
         total_ngh = 1
-        new = []
+        new = dict()
+        for i in self.collision_set.get(state):
+            v = state.config[i]
+            ngh = [v] + self.graph.neighbors[v]
+            new[i] = ngh
+            total_ngh = total_ngh * len(ngh)
+
+        Ssample = [None] * len(state.config)
+        for index, v in enumerate(state.config):
+            if index not in self.collision_set.get(state):
+                policy_next = self.get_policy(index, v)
+                Ssample[index] = (policy_next)
+        Snghstate = [copy.deepcopy(Ssample) for _ in range(total_ngh)]
+        new_tuple = [(key, value) for key, value in new.items()]
+        heapq.heapify(new_tuple)  # 这样在递归的时候可以去掉
+        Snghstates = self.insert_neigh(new_tuple, Snghstate, state)
+        Sngh = []
+        for states in Snghstates:
+            Sngh.append(joint_Node(states, state, state.g + self.get_edge(state.config, states)))
+        return Sngh
 
     def get_policy(self, agent, vertex):
         """
@@ -124,8 +162,33 @@ class m_star:
         """
         return self.policy_table[agent][vertex[0]][vertex[1]]
 
-    def backPropagation(self, sl, C_k):
-        pass
+    def backPropagation(self, s, C_k):
+        """
+        Doing backpropagation
+        """
+        if self.IsSubset(C_k, self.collision_set.get(s)):
+            return
+        self.UnionColset(s,C_k)
+        self.reopen(s)
+
+        if s.config == self.v_init:
+            return
+        for parent_s in self.backprop.get(s):
+            self.backPropagation(parent_s, C_k)
+
+    def IsSubset(self, C_k, curr_C):
+        """
+        As its name
+        """
+        return C_k.issubset(curr_C)
+
+    def reopen(self, s):
+        """
+        Reinsert one node to open set
+        """
+        heapq.heappush(self.Open, (s.g + self.get_h(s), s))
+        if s in self.closed:
+            self.closed.remove(s)
 
     def Add_toBackprop(self, curr_s, ngh_s):
         """
@@ -135,4 +198,92 @@ class m_star:
             self.backprop[ngh_s] = set()
         self.backprop[ngh_s].add(curr_s)
         return
+
+    def get_edge(self, config_from, config_to):
+        """
+        Return edge cost
+        """
+        cost = 0
+        for i in range(len(self.v_f)):
+            if not (self.v_f[i] == config_from[i] == config_to[i]):
+                cost += 1
+        return cost
+
+    def insert_neigh(self, new_tuple, Snghstate, sk):
+        """
+        Insert collision neighbours to the following places
+        """
+        if len(new_tuple) == 0:
+            return Snghstate
+
+        re_nghs = []
+        agent, nghs = heapq.heappop(new_tuple)
+        division = self.divide_list(Snghstate, len(nghs))  # 把sngh拆分成nghs数量等同的部分
+
+        for ngh, sub_Snghs in enumerate(division): #表示特定ngh的下标和分配到要填充的分部分
+            v = nghs[ngh]
+            for j in range(len(sub_Snghs)):  # j 表示要插的位置
+                sub_Snghs[j][agent] = v
+
+            new_tuple_copy = copy.deepcopy(new_tuple) #跟list一样防止反复调用
+            re_nghs += tuple(self.insert_neigh(new_tuple_copy, sub_Snghs, sk))
+
+        return re_nghs
+
+    def divide_list(self, my_list, num_parts):
+        """
+        Divide the list to certain part to insert the neighbours
+        """
+        avg = len(my_list) // num_parts
+        remainder = len(my_list) % num_parts
+        result = [copy.deepcopy(my_list[i * avg + min(i, remainder):(i + 1) * avg + min(i + 1, remainder)]) for i in
+                  range(num_parts)]
+        return result
+
+    def collision_detect(self, state):
+        """
+        Return a collision set
+        """
+        Occupied = dict()
+        C_i = set()
+        for index, v in enumerate(state.config):
+            if Occupied.get(v) is None:
+                Occupied[v] = set()
+                Occupied[v].add(index)
+            else:
+                C_i.add(index)
+                for i in Occupied.get(v):
+                    C_i.add(i)
+        return C_i
+
+    def UnionColset(self, s, C_i):
+        """
+        Union the collision set
+        """
+        c = self.collision_set.get(s)
+        if c is None:
+            self.collision_set[s] = C_i
+        else:
+            union = C_i.union(c)
+            self.collision_set[s] = union
+
+    def compare(self, s, C_i):
+        """
+        Compare if a new node is better
+        """
+        if len(C_i) != 0:
+            return False
+        s_before = self.Explored.get(s.config)
+        if s_before is None:
+            self.Explored[s.config] = s
+            return True
+        else:
+            if s.g < s_before.g:
+                self.Explored[s.config] = s
+                return True
+            return False
+
+
+
+
 
